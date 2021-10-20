f8abb844646c6fdbb1331b7ae2e55d8d46c19681
nagablaze <90331442+nagablaze@users.noreply.github.com>
2021-10-20 19:46:36 +0530

    Update GameController.java
    commit changes

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -18,6 +18,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  * Controller to navigate the buttons on the game pages
  * new commit on github-testing
  * new commit on me
+ * new commit for new pipeline
  */
 @Controller
 @RequestMapping("/game")
